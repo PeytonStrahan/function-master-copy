641fe99c06fe61f99553b3fa203aa090e505b05a
PeytonStrahan <peytonstrahan@outlook.com>
2024-09-18 16:03:25 -0500

    function-master.js done up to step 19

--- function-master.js
+++ function-master.js
@@ -4,6 +4,18 @@
 
 function objectValues(object) {
     // code
+
+    // create new empty array
+    var newArr = [];
+
+    // loop through input object
+    for (let key in object) {
+        // push each current object property into the new array
+        newArr.push(object[key]);
+    }
+
+    // return the new array
+    return newArr;
 } 
 
 //////////////////////////////////////////////////////////////////////
@@ -11,7 +23,20 @@ function objectValues(object) {
 //////////////////////////////////////////////////////////////////////
 
 function keysToString(object) {
+    // create new empty string
+    var newString = "";
 
+    // loop through input object
+    for (let key in object) {
+        // concatenate each current object key and a space string onto the new string
+        newString += key + " ";
+    }
+
+    // remove the last space from the new string
+    newString = newString.slice(0, newString.length - 1);
+
+    // return the new string
+    return newString;
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -19,7 +44,23 @@ function keysToString(object) {
 //////////////////////////////////////////////////////////////////////
 
 function valuesToString(object) {
-    
+    // create new empty string
+    var newString = "";
+
+    // loop through input object
+    for (let key in object) {
+        // determine if the current object property is a string
+        if (typeof object[key] === "string") {
+            // concatenate the current object property and a space string onto the new string if so
+            newString += object[key] + " ";
+        }
+    }
+
+    // remove the last space from the new string
+    newString = newString.slice(0, newString.length - 1);
+
+    // return the new string
+    return newString;
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -27,7 +68,16 @@ function valuesToString(object) {
 //////////////////////////////////////////////////////////////////////
 
 function arrayOrObject(collection) {
-    
+    // determine if the current collection is an array
+    if (Array.isArray(collection)) {
+        // return "array" if so
+        return "array";
+    }
+    // otherwise, determine if the current collection is an object
+    else if (typeof collection === "object") {
+        // return "object" if so
+        return "object";
+    }
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -35,7 +85,8 @@ function arrayOrObject(collection) {
 //////////////////////////////////////////////////////////////////////
 
 function capitalizeWord(string) {
-    
+    // return the result of concatenating the capitalized first character of the word to the front of another version of that same word that had its first character excluded via a slice method
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -43,7 +94,28 @@ function capitalizeWord(string) {
 //////////////////////////////////////////////////////////////////////
 
 function capitalizeAllWords(string) {
-    
+    // create new empty string
+    var newString = "";
+
+    // create a strings array variable and assign to it the array made from using .split on the input string to separate all strings in this new array by spaces
+    var stringsArr = string.split(/(\s)/);
+
+    // loop through the strings array
+    for (let i = 0; i < stringsArr.length; i++) {
+        console.log(stringsArr)
+        console.log(newString)
+        // determine if the current string is not a space
+        if (stringsArr[i] !== " ") {
+            // concatenate the new string with the result of concatenating the capitalized first character of the current word to the front of another version of that same word that had its first character excluded via a slice method
+            newString += stringsArr[i].charAt(0).toUpperCase() + stringsArr[i].slice(1);
+        } else {
+            // otherwise, concatenate the space onto the new string
+            newString += stringsArr[i];
+        }
+    }
+
+    // return the new string
+    return newString;
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -51,7 +123,11 @@ function capitalizeAllWords(string) {
 //////////////////////////////////////////////////////////////////////
 
 function welcomeMessage(object) {
+    // create a capitalized name string by calling the capitalizeWord function with the inputted object's name property
+    var capName = capitalizeWord(object.name);
 
+    // return a welcome message string concatenated with the capitalized name string
+    return "Welcome " + capName + "!";
 }
 
 //////////////////////////////////////////////////////////////////////
